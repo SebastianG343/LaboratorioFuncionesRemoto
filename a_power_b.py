8759a135aa2a79889e4db24be9223639f31835a9
SebastianG343 <juan.gelvez.2018@upb.edu.co>
2019-03-30 20:29:16 -0500

    a_power_b

---
+++ a_power_b.py
@@ -0,0 +1,15 @@
+def a_power_b():
+    a=int(input("Pls digite un numero"))
+    b=int(input("Pls digite otro numero"))
+    z=a**b
+    print(z)
+a_power_b()
+
+
+    
+
+
+        
+        
+
+        
