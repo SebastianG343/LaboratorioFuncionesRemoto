3d91962339543fb861809ed1462d633f1997c368
SebastianG343 <juan.gelvez.2018@upb.edu.co>
2019-03-30 23:30:31 -0500

    is_prime2

---
+++ is_prime2.py
@@ -0,0 +1,17 @@
+def perfect_number():
+    x=0
+    k=0
+    l=[]
+    n=int(input("Digite un numero"))
+    for i in range(0,n):
+        if n%i==0:
+            x+=1
+            i.append(l)
+            print(l)
+            for len in (l):
+                k+=1
+                if k==n:
+                    print("Es perfecto")
+                else:
+                    print("No es perfecto")
+perfect_number()
