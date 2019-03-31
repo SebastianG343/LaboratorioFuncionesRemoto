43e7e88bcab8299026f65181ae1055df373cdd7d
SebastianG343 <juan.gelvez.2018@upb.edu.co>
2019-03-30 21:22:04 -0500

    a_power_b3

---
+++ a_power_b3.py
@@ -0,0 +1,21 @@
+def a_power_b():
+    print("Si digita 0 dejara de pedir numeros")
+    a=int(input("Pls digite un numero"))
+    b=int(input("Pls digite otro numero"))
+    while a!=0 or b!=0:
+        a=int(input("Pls digite un numero"))
+        b=int(input("Pls digite otro numero"))
+        z=(a**b)
+        print("a:",a,"b:",b,"R:",z)
+a_power_b()
+
+
+
+
+    
+
+
+        
+        
+
+        
